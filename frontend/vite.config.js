bd0d7a38fd18c98fec128d08f644c9001fde03f9
Mark Gelman <gelman_mark@Mac.fritz.box>
2026-03-02 22:03:00 +0100

    fix: remove vite optimizedeps to fix vercel rollup compilation crash

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,21 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 5173,
     watch: {
-    }
-  },
-  optimizeDeps: {
-    noDiscovery: true
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-is', 'prop-types']
+      usePolling: true
     }
   }
 })
